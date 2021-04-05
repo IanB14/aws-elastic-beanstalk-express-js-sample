f976498748763f9c4b830092a07bb5b50148e15f
Ian Burke <ian_burke@hotmail.com>
2021-04-05 12:59:54 +0100

    Full Pipeline Test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Updating Hello World for push to git.'));
+app.get('/', (req, res) => res.send('Updating Hello World again for last module.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
